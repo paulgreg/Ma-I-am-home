f52cf440519e1b2de5bbbeaf299d981f80403c73
Grégory Paul <paulgreg@gmail.com>
2019-08-12 18:33:57 +0200

    Somes updates (now it works on the definitive box)

--- Ma-I-am-home.ino
+++ Ma-I-am-home.ino
@@ -2,37 +2,42 @@
 #include <WiFiClientSecure.h>
 #include "parameters.h"
 
-#define LED 2
+// default LED on board is 2
+// I’m using here GPIO 5 to connect a LED with a 220 ohm resistor
+#define LED 5
 #define BUTTON 13
 #define WAIT 100
 #define REQUEST_TIMEOUT 5000
 
-#define HOST "smsapi.free-mobile.fr"
-#define HOST_CERTIFICATE_FINGERPRINT "3B 5C 64 35 F5 28 BF 1C FA 96 DC E7 65 B1 E1 B3 2E 84 35 77";
-
-const char  host_certificate_fingerprint[] PROGMEM = HOST_CERTIFICATE_FINGERPRINT
+const char host_certificate_fingerprint[] PROGMEM = HOST_CERTIFICATE_FINGERPRINT
 
 void setup() {
   pinMode(LED, OUTPUT);
-  digitalWrite(LED, HIGH);
   pinMode(BUTTON, INPUT);
   Serial.begin(74880);
-  Serial.println("");
+  while (!Serial);
   Serial.println("Connected button");
+  blinkOnce();
+  
+  connectToWifi();
+  for (int i = 0; i < URLS_SIZE; i++) {
+    blinkOnce();
+    get(HOST, URLS[i]);
+    blinkOnce();
+  }
+  WiFi.mode(WIFI_OFF);
+
+  longBlinkOnce();
+
+  Serial.println("Sleeping");
+  ESP.deepSleep(0);
 }
 
 void loop() {
-  delay(WAIT);
-  if (digitalRead(BUTTON) == LOW) {
-    connectToWifi();
-    get(HOST, URL_1);
-    //get(HOST, URL_2);
-    WiFi.mode(WIFI_OFF);
-  }
+  // sleeping so wont get here
 }
 
 void connectToWifi() {
-
   Serial.println("");
   Serial.print("connecting to ");
   Serial.println(WIFI_SSID);
@@ -41,17 +46,15 @@ void connectToWifi() {
   WiFi.mode(WIFI_STA);  
   WiFi.begin(WIFI_SSID, WIFI_PASSWORD);
   while (WiFi.status() != WL_CONNECTED) {
-    delay(500);
+    blinkOnce();
     Serial.print(".");
   }
-  digitalWrite(LED, LOW);
   Serial.println("");
   Serial.println("wifi connected");
   Serial.print("IP address: ");
   Serial.println(WiFi.localIP());
   Serial.println("");
 }
-
 
 boolean get(const char* host, const char* url) {
   WiFiClientSecure client;
@@ -64,6 +67,7 @@ boolean get(const char* host, const char* url) {
 
   unsigned int retries = 5;
   while(!client.connect(host, 443) && (retries-- > 0)) {
+    blinkOnce();
     Serial.print(".");
   }
 
@@ -90,11 +94,21 @@ boolean get(const char* host, const char* url) {
     }
   }
 
-  if (client.connected()) {
-    String line = client.readStringUntil('\n');
-    Serial.print("< ");
-    Serial.println(line);
-    status = true;
-  }
-  return status;
+  return client.connected();
+}
+
+void blinkOnce() {
+  digitalWrite(LED, HIGH);
+  delay(100);
+  digitalWrite(LED, LOW);
+  delay(100);
+}
+
+void longBlinkOnce() {
+  for(int i = 0; i < 3; i++) {
+    digitalWrite(LED, HIGH);
+    delay(500);
+    digitalWrite(LED, LOW);
+    delay(500);
+  }  
 }
--- README.md
+++ README.md
@@ -2,18 +2,23 @@
 
 That project is a connected button which, when pressed, send a SMS to people. It’s used by my son, to text me when he’s back from school.
 
+## Instructions
 
-## ESP8266 Connections
+### ESP8266 Connections
 
 - VCC to 3.3
 - Ground to ground
-- 0.1 nF capacitor between ground and VCC (the closest to ESP)
 - RX from FTDI to TX on ESP
 - TX from FTDI to RX on ESP
 - DTR from FTDI to GPIO 0 on ESP
 - RST from FTDI to RST on ESP
-- CH_PD from ESP to 10 Kohms HIGH
-- GPIO 15 from ESP to 10 Kohms LOW
+- CH_PD from ESP to VCC
+- GPIO 15 from ESP to ground 
+- 
+## Software
+
+copy `parameters.h.dist` to `parameters.h` and replace wifi SSID and password. You can use SMS free mobile API but it can only send SMS to your mobile phone, not any random number. You would have to change host and URL if you’re using another celullar provider.
+Beware, you need to extract SHA1 TLS certificate finterprint and update it in order to make the TLS connection work.
 
 ## links
 
@@ -21,4 +26,5 @@ That project is a connected button which, when pressed, send a SMS to people. It
   * [A Beginner's Guide to the ESP8266](https://tttapa.github.io/ESP8266/Chap01%20-%20ESP8266.html)
   * [ESP8266 NodeMCU HTTPS Secured POST Request](https://circuits4you.com/2019/02/08/esp8266-nodemcu-https-secured-post-request/)
   * [ESP8266 Wi-Fi Button – DIY Amazon Dash Button Clone](https://randomnerdtutorials.com/esp8266-wi-fi-button-diy-amazon-dash-button-clone/)
+  * [ESP8266 Pinout Reference](https://randomnerdtutorials.com/esp8266-pinout-reference-gpios/)
   * [Free Mobile SMS API](https://www.freenews.fr/freenews-edition-nationale-299/free-mobile-170/nouvelle-option-notifications-par-sms-chez-free-mobile-14817)
